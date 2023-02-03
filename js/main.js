8561c719b89555072fcec9426b7280aabf81ca0b
knlcker <nickswanenberg@gmail.com>
2023-02-03 13:45:31 +0100

    js push

--- js/main.js
+++ js/main.js
@@ -1,39 +1,40 @@
 const rangeChecker = document.getElementById('myRange');
+var checker = 0;
 
 const slidePalooza = () => {
-    
+
     const rangeChecker = document.getElementById('myRange');
     console.log(rangeChecker.value)
     
     if(rangeChecker.value > 99){
         var about = document.getElementById('about-text3')
-        about.classList.toggle('show');
+        about.classList.add('show');
         var about = document.getElementById('tags-Creator');
-        about.classList.toggle('show');
+        about.classList.add('show');
         var about = document.getElementById('content-container');
-        about.classList.toggle('show');
+        about.classList.add('show');
         var about = document.getElementById('content-container');
-        about.classList.toggle('show');
+        about.classList.add('show');
     }
     else if(rangeChecker.value > 74){
         var about = document.getElementById('about-text3')
-        about.classList.toggle('show');
+        about.classList.add('show');
         var about = document.getElementById('tags-Design');
-        about.classList.toggle('show');
+        about.classList.add('show');
     }
     else if(rangeChecker.value > 49){
         var about = document.getElementById('about-text2')
-        about.classList.toggle('show');
+        about.classList.add('show');
         var about = document.getElementById('tags-Java');
-        about.classList.toggle('show');
+        about.classList.addEventListener('show');
         var about = document.getElementById('my-work-title-content');
-        about.classList.toggle('show');
+        about.classList.add('show');
         var about = document.getElementById('my-work-title-content-img');
-        about.classList.toggle('show');
+        about.classList.add('show');
     }
     else if(rangeChecker.value > 24){
         var about = document.getElementById('about-text1');
-        about.classList.toggle('show');
+        about.classList.add('show');
     }
     
  
